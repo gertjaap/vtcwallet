8c56277b8ba4a3745a5c7646f44644463b7f64f1
Gert-Jaap Glasbergen <gj@getminute.com>
2017-09-18 20:56:44 +0200

    Added block explorer methods for XPUBs

--- middleware/routes/blockexplorer.js
+++ middleware/routes/blockexplorer.js
@@ -1,11 +1,25 @@
 var express = require('express');
+var bitcoin = require('bitcoinjs-lib');
 var router = express.Router();
 const async = require('async');
+
+var vertcoinNetwork =  {
+  messagePrefix: 'Vertcoin Signed Message:\n',
+  bip32: {
+    public: 0x0488b21e,
+    private: 0x0488ade4
+  },
+  pubKeyHash: 0x47,
+  scriptHash: 0x05,
+  wif: 0x80
+};
+
+
 /* GET users listing. */
 router.get('/addressBalance/:addr', function(req, res, next) {
   var balance = 0;
 
-  var expandTxo = function(txo, callback) {
+  var expandTxoLocal = function(txo, callback) {
     router.blockchainIndexing.db.get("txo-" + txo.txid + "-" + txo.vout, function (err, value) {
       router.blockchainIndexing.db.get("txo-" + txo.txid + "-" + txo.vout + "-spent", function (err, spent) {
         if(value) {
@@ -18,61 +32,198 @@ router.get('/addressBalance/:addr', function(req, res, next) {
     });
   };
 
-  var queue = async.queue(expandTxo, 10);
+  var queue = async.queue(expandTxoLocal, 10);
   queue.drain = function() {
-    res.json({balance:balance});
+    res.json({txos:expandedTxos});
   }
-  var count = 0;
-  router.blockchainIndexing.db.createReadStream({
-    start: req.params.addr + "-txo-00001",
-    end: req.params.addr + "-txo-99999"
-  })
-  .on('data', function(data) {
-    queue.push(JSON.parse(data.value));
-    count++;
-  })
-  .on('end', function() {
-    if(count == 0) queue.drain();
+  getAddressTxos(req.params.addr, function(txos) {
+    for(var i = 0; i < txos.length; i++)
+    {
+      queue.push(txos[i]);
+    }
+    if(txos.length === 0) queue.drain();
   });
 });
+
+router.get('/xpubBalance/:xpub', function(req, res, next) {
+
+  var balance = 0;
+
+  var expandTxoLocal = function(txo, callback) {
+    expandTxo(txo, function(txoObject) {
+      if(txoObject && !txoObject.spent)
+        balance += txoObject.value;
+
+      callback();
+    });
+  };
+
+  var queue = async.queue(expandTxoLocal, 1);
+  var node = bitcoin.HDNode.fromBase58(req.params.xpub,vertcoinNetwork);
+
+  var maxNode = -1;
+  var expandAddress = function(index, callback) {
+    var derived = node.derive(index);
+    address = derived.getAddress(vertcoinNetwork);
+    getAddressTxos(address, function(txos) {
+      for(var i = 0; i < txos.length; i++)
+      {
+        txos[i].address = address;
+        queue.push(txos[i]);
+      }
+      if(txos.length > 0) {
+        maxNode++;
+        addressQueue.push(maxNode);
+      }
+      callback();
+    });
+  }
+
+
+
+  var addressQueue = async.queue(expandAddress, 1);
+  var sentResult = false;
+  queue.drain = function() {
+    if(addressQueue.length() == 0 && !sentResult)
+    {
+      sentResult = true;
+      res.json({balance:balance});
+    }
+  }
+
+  addressQueue.drain = function() {
+    if(queue.length() == 0 && !sentResult)
+    {
+      sentResult = true;
+      res.json({balance:balance});
+    }
+  }
+
+  for(var i = 0; i < 10; i++)
+  {
+    maxNode++;
+    addressQueue.push(maxNode);
+  }
+});
+
+router.get('/xpubTxos/:xpub', function(req, res, next) {
+
+  var expandedTxos = [];
+
+  var expandTxoLocal = function(txo, callback) {
+    expandTxo(txo, function(txoObject) {
+      if(txoObject)
+        expandedTxos.push(txoObject);
+
+      callback();
+    });
+  };
+
+  var queue = async.queue(expandTxoLocal, 1);
+  var node = bitcoin.HDNode.fromBase58(req.params.xpub,vertcoinNetwork);
+
+  var maxNode = -1;
+  var expandAddress = function(index, callback) {
+    var derived = node.derive(index);
+    address = derived.getAddress(vertcoinNetwork);
+    getAddressTxos(address, function(txos) {
+      for(var i = 0; i < txos.length; i++)
+      {
+        txos[i].address = address;
+        queue.push(txos[i]);
+      }
+      if(txos.length > 0) {
+        maxNode++;
+        addressQueue.push(maxNode);
+      }
+      callback();
+    });
+  }
+
+
+
+  var addressQueue = async.queue(expandAddress, 1);
+  var sentResult = false;
+  queue.drain = function() {
+    if(addressQueue.length() == 0 && !sentResult)
+    {
+      sentResult = true;
+      res.json({txos:expandedTxos});
+    }
+  }
+
+  addressQueue.drain = function() {
+    if(queue.length() == 0 && !sentResult)
+    {
+      sentResult = true;
+      res.json({txos:expandedTxos});
+    }
+  }
+
+  for(var i = 0; i < 10; i++)
+  {
+    maxNode++;
+    addressQueue.push(maxNode);
+  }
+});
+
+var getAddressTxos = function(addr, callback) {
+  var txos = [];
+  router.blockchainIndexing.db.createReadStream({
+    start: addr + "-txo-00001",
+    end: addr + "-txo-99999"
+  })
+  .on('data', function(data) {
+    txos.push(JSON.parse(data.value));
+  })
+  .on('end', function() {
+    callback(txos);
+  });
+}
+
+var expandTxo = function(txo, callback) {
+  router.blockchainIndexing.db.get("txo-" + txo.txid + "-" + txo.vout, function (err, value) {
+    router.blockchainIndexing.db.get("txo-" + txo.txid + "-" + txo.vout + "-spent", function (err, spent) {
+      var txoObject = null;
+
+      if(value) {
+        txoObject = { value: parseFloat(value) };
+        txoObject.txid = txo.txid;
+        txoObject.vout = txo.vout;
+        if(spent) {
+          txoObject.spent = true;
+          txoObject.spentTxID = spent;
+        }
+      }
+      callback(txoObject);
+    });
+  });
+};
 
 router.get('/addressTxos/:addr', function(req, res, next) {
   var expandedTxos = [];
 
-  var expandTxo = function(txo, callback) {
-    router.blockchainIndexing.db.get("txo-" + txo.txid + "-" + txo.vout, function (err, value) {
-      router.blockchainIndexing.db.get("txo-" + txo.txid + "-" + txo.vout + "-spent", function (err, spent) {
-        if(value) {
-          var txoObject = { value: parseFloat(value) };
-          txoObject.txid = txo.txid;
-          txoObject.vout = txo.vout;
-          if(spent) {
-            txoObject.spent = true;
-            txoObject.spentTxID = spent;
-          }
-          expandedTxos.push(txoObject);
-        }
-        callback();
-      });
+  var expandTxoLocal = function(txo, callback) {
+    expandTxo(txo, function(txoObject) {
+      if(txoObject)
+        expandedTxos.push(txoObject);
+
+      callback();
     });
   };
 
-  var queue = async.queue(expandTxo, 10);
+  var queue = async.queue(expandTxoLocal, 10);
   queue.drain = function() {
     res.json({txos:expandedTxos});
   }
-  var count = 0;
-  router.blockchainIndexing.db.createReadStream({
-    start: req.params.addr + "-txo-00001",
-    end: req.params.addr + "-txo-99999"
-  })
-  .on('data', function(data) {
-    queue.push(JSON.parse(data.value));
-    count++;
-  })
-  .on('end', function() {
-    if(count == 0) queue.drain();
+  getAddressTxos(req.params.addr, function(txos) {
+    for(var i = 0; i < txos.length; i++)
+    {
+      queue.push(txos[i]);
+    }
+    if(txos.length === 0) queue.drain();
   });
+
 });
 
 router.get('/tx/:txid', function(req, res, next) {
